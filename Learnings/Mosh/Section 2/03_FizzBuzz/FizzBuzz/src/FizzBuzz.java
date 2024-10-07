82dc01525c1112777154c1e33d8bcd0b2c00a1a0
Elsa Rebello <elsarebello@gmail.com>
2024-10-07 14:57:17 +0530

    Code commit

---
+++ Learnings/Mosh/Section 2/03_FizzBuzz/FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        String output;
+
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println("FizzBuzz");
+        }
+        else if (number % 5 == 0) {
+            System.out.println("Fizz");
+        }
+        else if(number % 3 == 0) {
+            System.out.println("Buzz");
+        } 
+        else {
+            System.out.println(number);
+        }
+    }
+}
